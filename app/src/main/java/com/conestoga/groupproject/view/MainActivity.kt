2c0521074c0cbce8d9356296bacc49ffd65afff6
ktsenthil2012@gmail.com <SenthilGit@123>
2023-03-12 18:23:04 -0400

    Sign in and sign up activity code

--- app/src/main/java/com/conestoga/groupproject/view/MainActivity.kt
+++ app/src/main/java/com/conestoga/groupproject/view/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.conestoga.groupproject.view
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.Recycler
 import com.conestoga.groupproject.R
 import com.conestoga.groupproject.data.entities.AppOrder
 import com.conestoga.groupproject.data.entities.Product
@@ -21,6 +17,7 @@ import com.conestoga.groupproject.view.adapters.WrapContentLinearLayoutManager
 import com.conestoga.groupproject.view.checkout.ProductDetail
 import com.conestoga.groupproject.view.orders.OrdersActivity
 import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -44,15 +41,19 @@ class MainActivity : AppCompatActivity() {
         val query = FirebaseDatabase.getInstance().reference.child("watches")
         val options =
             FirebaseRecyclerOptions.Builder<Product>().setQuery(query, Product::class.java).build()
-        binding.user = intent.getSerializableExtra("user") as User
+//        binding.user = intent.getSerializableExtra("user") as User
+
+        val user = FirebaseAuth.getInstance().currentUser
 
         binding.ivBell.setOnClickListener {
             val intent = Intent(this, OrdersActivity::class.java)
-            intent.putExtra("userId", binding.user?.id)
+//            intent.putExtra("userId", binding.user?.id)
+            intent.putExtra("userId", user?.uid)
             startActivity(intent)
         }
 
-        listenForOrders(binding.user?.id.toString())
+//        listenForOrders(binding.user?.id.toString())
+        listenForOrders(user?.uid.toString())
 
         binding.rvProducts.apply {
             layoutManager = WrapContentLinearLayoutManager(this@MainActivity)
---
+++ app/src/main/java/com/conestoga/groupproject/view/auth/SignInActivity.kt
@@ -0,0 +1,60 @@
+package com.conestoga.groupproject.view.auth
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.conestoga.groupproject.databinding.ActivitySignInBinding
+import com.conestoga.groupproject.view.MainActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.textView.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.button.setOnClickListener {
+//            Toast.makeText(this, "Button Clicked..!!", Toast.LENGTH_SHORT).show()
+
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passET.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception?.message, Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if(firebaseAuth.currentUser != null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
